12da2ccc3182ecab15eb3e814ddc96ef2576a079
Shourya Kumar Srivastava <42997663+shouryasrivastava@users.noreply.github.com>
2023-02-18 23:15:52 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,85 @@
+import socket
+import threading 
+import json
+
+IP = "10.120.70.105"#socket.gethostbyname(socket.gethostname())
+PORT = 6677
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
+database = {}
+cohorts = {}
+
+def handle_client(conn, addr):
+	print(f"[NEW CONNECTIONS] {addr} connected.")
+
+	connected = True
+	while connected:
+		msg = conn.recv(SIZE).decode(FORMAT)
+		tokens = msg.split()
+		command = tokens[0]
+		customer = tokens[1]
+		#command,*argus = msg.split()
+		if msg == DISCONNECT_MSG:
+			connected = False
+
+		print(f"[{addr}] {msg}")
+		#msg = f"Msg received: {msg}"
+		if command == "open":
+			balance = int(tokens[2])
+			ipv4_address = tokens[3]
+			port_b = int(tokens[4])
+			port_p = int(tokens[5])
+			#customer, balance, ip_address, port_b, port_p = argus
+			if customer in database:
+				msg = "FAILURE"
+			else:
+				database[customer] = (float(balance), ipv4_address, int(port_b), int(port_p))
+				msg = "SUCCESS"
+		
+		elif command == "new-cohort":
+			n = int(tokens[2])
+
+			if n < 2 or n > len(database):
+				msg = "FAILURE"
+			else:
+				import random
+				cohort = set([customer])
+				while len(cohort) < n:
+					cohort.add(random.choice(list(database.keys())))
+				cohort_info = []
+				for c in cohort:
+					cohort_info.append((c,) + database[c][1:])
+				cohorts[customer] = cohort
+				
+				msg = f"SUCCESS\n " + "\n".join([f"{c[0]} {c[1]} {c[2]} {c[3]}" for c in cohort_info])
+            
+		elif command == "delete-cohort":
+			if customer not in database:
+				msg = "FAILURE"
+			elif customer not in cohorts:
+				msg = "FAILURE"		
+
+		conn.send(msg.encode(FORMAT))
+
+
+	conn.close()
+
+
+def main():
+	print("[STARTING] Server is starting...")
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind(ADDR)
+	server.listen()
+	print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+
+	while True:
+		conn, addr = server.accept()
+		thread = threading.Thread(target=handle_client, args=(conn,addr))
+		thread.start()
+		#print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+if __name__ == "__main__":
+	main()
